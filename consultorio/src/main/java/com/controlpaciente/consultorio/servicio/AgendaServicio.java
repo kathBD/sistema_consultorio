108b712cebf7560c622662053fb0de301298757f
katherine Bonilla <felicidadkg@gmail.com>
2023-02-12 03:19:41 -0500

    se realizo interface agenda y servicio

--- consultorio/src/main/java/com/controlpaciente/consultorio/servicio/AgendaServicio.java
+++ consultorio/src/main/java/com/controlpaciente/consultorio/servicio/AgendaServicio.java
@@ -1,4 +1,61 @@
 package com.controlpaciente.consultorio.servicio;
 
-public class AgendaServicio {
+import com.controlpaciente.consultorio.model.Agenda;
+import com.controlpaciente.consultorio.model.Cita;
+import com.controlpaciente.consultorio.model.Paciente;
+
+import java.util.List;
+
+public class AgendaServicio implements IAgenda{
+
+
+    @Override
+    public Paciente obtAgendaPorId(String idAgenda) {
+        return null;
+    }
+
+    @Override
+    public List<Agenda> obtenertodosAgendas() {
+        return null;
+    }
+
+    @Override
+    public void agregarAgenda(Agenda agenda) {
+
+    }
+
+    @Override
+    public Agenda actualizarAgenda(Agenda agenda, String idagenda) {
+        return null;
+    }
+
+    @Override
+    public List<Cita> obtenertodasConsultas(Agenda agenda) {
+        return null;
+    }
+
+    @Override
+    public Cita actualizarCita(Agenda agenda, Cita cita, int idCita) {
+        return null;
+    }
+
+    @Override
+    public Cita obtenerCitaporID(Agenda agenda, int idCita) {
+        return null;
+    }
+
+    @Override
+    public void cancelarCita(Agenda agenda, int idCita) {
+
+    }
+
+    @Override
+    public void aceptarCita(Agenda agenda, int idCita) {
+
+    }
+
+    @Override
+    public void asiganarPaciente(String idAgenda, int idCita, Paciente paciente) {
+
+    }
 }
--- consultorio/src/main/java/com/controlpaciente/consultorio/servicio/IAgenda.java
+++ consultorio/src/main/java/com/controlpaciente/consultorio/servicio/IAgenda.java
@@ -1,4 +1,22 @@
 package com.controlpaciente.consultorio.servicio;
 
+import com.controlpaciente.consultorio.model.Agenda;
+import com.controlpaciente.consultorio.model.Cita;
+import com.controlpaciente.consultorio.model.Paciente;
+
+import java.util.List;
+
 public interface IAgenda {
+    Paciente obtAgendaPorId(String idAgenda);
+    List<Agenda> obtenertodosAgendas();
+    void agregarAgenda(Agenda agenda);
+
+    Agenda actualizarAgenda(Agenda agenda, String idagenda);
+
+    List<Cita> obtenertodasConsultas(Agenda agenda );
+    Cita actualizarCita(Agenda agenda, Cita cita,int idCita);
+    Cita obtenerCitaporID(Agenda agenda,int idCita);
+    void cancelarCita(Agenda agenda,int idCita);
+    void aceptarCita(Agenda agenda,int idCita);
+    void asiganarPaciente(String idAgenda,int idCita, Paciente paciente);
 }
